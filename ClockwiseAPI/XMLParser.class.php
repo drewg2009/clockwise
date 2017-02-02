90d9b10d7d2ce1852b7ce856f26d487c57164776
Seth Delbridge <sadelbrid@gmail.com>
2017-02-02 11:00:59 -0500

    Added XML parser takes URL input

--- ClockwiseAPI/XMLParser.class.php
+++ ClockwiseAPI/XMLParser.class.php
@@ -1,22 +1,22 @@
 <?php
 class XMLParser
 {
-    public static function get_atom_entries($input, $limit){
+    public static function get_atom_entries($url, $limit){
     	$entries = array();
-    	$xml=simplexml_load_file(input) or return $entries;
-	for ($x = 0; $x < limit; $x++) {
-		$entries[x] = $xml->entry[x]->title;
-	}
-	return $entries;
+    	$xml=simplexml_load_file($url) or return $entries;
+		for ($x = 0; $x < $limit; $x++) {
+			$entries[x] = $xml->entry[x]->title;
+		}
+		return $entries;
     }
 
-    public static function get_rss_item($input, $limit){
+    public static function get_rss_item($url, $limit){
     	$items = array();
-    	$xml=simplexml_load_file(input) or return $items;
-	for ($x = 0; $x < limit; $x++) {
-		$items[x] = $xml->item[x]->title;
-	}
-	return $items;
+    	$xml=simplexml_load_file($url) or return $items;
+		for ($x = 0; $x < $limit; $x++) {
+			$items[x] = $xml->item[x]->title;
+		}
+		return $items;
     }
 }
 ?>
