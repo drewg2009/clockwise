07e0b0f9a992f7f71ab92bf87aa0f30335919076
Seth Delbridge <sadelbrid@gmail.com>
2017-05-22 11:00:42 -0400

    Updated Speech Service

--- Droid/SpeechService.cs
+++ Droid/SpeechService.cs
@@ -2,6 +2,12 @@
 using Android.App;
 using Android.OS;
 using Android.Content;
+using Geolocator.Plugin;
+using Android.Content.PM;
+using Clockwise.Helpers;
+using Android.Widget;
+using Java.Util;
+
 namespace Clockwise.Droid
 {
 	[Service]
@@ -17,6 +23,73 @@ namespace Clockwise.Droid
 
 		}
 
+		public override StartCommandResult OnStartCommand(Intent intent, StartCommandFlags flags, int startId)
+		{
+			int alarm_index = intent.GetIntExtra("alarm_index", -1);
+
+			if (alarm_index >= 0)
+			{
+				AlarmUtils.ClearNotification(alarm_index);
+
+				//Turn of alarm tone
+				//if(ServiceHelper.isMyServiceRunning(SongService.class, this)
+				if (SongService.intent != null)
+                    StopService(SongService.intent);
+				if (AlarmReceiver.player != null && AlarmReceiver.player.IsPlaying)
+				{
+					AlarmReceiver.player.Stop();
+					AlarmReceiver.player = null;
+				}
+
+				//Reschedule
+				Calendar calendar = Calendar.Instance;
+				calendar.TimeInMillis = Java.Lang.JavaSystem.CurrentTimeMillis();
+
+				int temp = int.Parse(Settings.GetAlarmField(alarm_index, Settings.AlarmField.Status));
+				if (temp == (int)Settings.AlarmStatus.ALARM_ON)
+				{
+					if (int.Parse(Settings.GetAlarmField(alarm_index, Settings.AlarmField.RepeatDays)) > 0)
+					{
+						AlarmUtils.SetTime(this, int.Parse(Settings.GetAlarmField(alarm_index, Settings.AlarmField.Hour)),
+						                   int.Parse(Settings.GetAlarmField(alarm_index, Settings.AlarmField.Minute)),
+						                   alarm_index, int.Parse(Settings.GetAlarmField(alarm_index, Settings.AlarmField.RepeatDays)), false);
+					}
+					else {
+						Settings.SetAlarmField(alarm_index, Settings.AlarmField.Status, "" + (int)Settings.AlarmStatus.ALARM_OFF);
+						if (ManageAlarms.instance != null)
+							ManageAlarms.instance.RefreshAlarms();
+					}
+				}
+
+			}
+			else {
+				Toast.MakeText(this, "bad index: " + alarm_index, ToastLength.Long).Show();
+			}
+
+			MakeRequest(alarm_index);
+			StopSelf();
+			return base.OnStartCommand(intent, flags, startId);
+		}
+
+		public async void MakeRequest(int index)
+		{
+			string request;
+			if (CheckSelfPermission(Android.Manifest.Permission.ReadExternalStorage) == Permission.Granted)
+			{
+				var locator = CrossGeolocator.Current;
+				locator.DesiredAccuracy = 50;
+				var position = await locator.GetPositionAsync(timeoutMilliseconds: 10000);
+				Console.WriteLine("Lat:{0}, Lon:{1}", position.Latitude, position.Longitude);
+				request = JSONRequestSerializer.GetInstance().GetJsonRequest(index, position.Latitude, position.Longitude);
+			}
+			else
+			{
+				request = JSONRequestSerializer.GetInstance().GetJsonRequest(index, 0, 0);
+			}
+
+			Console.WriteLine("\nrequest: " + request);
+
+		}
 		public override IBinder OnBind(Intent intent)
 		{
 			return null;
